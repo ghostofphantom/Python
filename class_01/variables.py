0f1351df85e883746279b5acf24205b947481173
ghostofphantom <itsmehassxn@gmail.com>
2025-10-22 17:34:57 +0500

    var

---
+++ class_01/variables.py
@@ -0,0 +1,51 @@
+
+# VARIBALES
+
+# name = "baddar"
+# num = 555
+
+
+# TYPE CASTING
+
+# STRING ,  NUMBER, FLOAT , LIST, TUPLE , SET, COMPLEX,
+
+
+# name = "This is String"
+
+# num = 77
+
+# flo = 77.0
+
+# type
+
+# print(type(flo))
+
+# num = '6'
+
+# num = str(6)
+# myVal = int("999")
+# fl = float(6)
+# print(type(fl))
+
+
+# ========= LEGAL ==========
+
+# _val = 54654
+# myvar = "alksjfa"
+# myva_klsjf = "aksjfasf"
+# Mysal = "jjj"
+# my44 = "99999"
+
+# ========= ILEGAL ==========
+
+#$ , 34243 , SPACE ,  print() , float
+
+
+# x = "orange"
+# y = "orange"
+# z = "orange"
+
+x = y = z = "h" "e" "r" 
+
+
+print(x , y, z)
