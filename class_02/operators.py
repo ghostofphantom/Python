f9f8f98a2fb41ba27656b139a7f8c1fd41027d5a
ghostofphantom <itsmehassxn@gmail.com>
2025-10-22 17:51:09 +0500

    operators

---
+++ class_02/operators.py
@@ -0,0 +1,46 @@
+# =========== Arthimatic Operators ==========
+
+
+# ==== ( + , - , * , / , % , ** , exponent ) === MATHEMATIC OPERATORS
+
+
+# print( 10 + 10 )
+# print( 10 - 10 )
+# print( 10 / 10 )
+# print( 10 * 10 )
+# print( 10 % 10 )
+# print( 10 ** 10 )
+
+# 5*5*5
+
+# print(5 ** 10)
+
+
+
+# ========== ASSIGNMENT OPERATORS =========
+
+# name = 10
+
+# x = 5
+
+# x = 2
+
+# print(x)
+
+# x += 5
+# print(x)
+
+
+# ========== Comparison OPERATORS =========
+
+# ( == ( 10 == 10)  ,,,, != ,,, > , < , >= , <= ,  )
+
+
+
+# print(10 == 10) 
+# print(20 > 10) 
+# print(20 < 10) 
+# print(20 != 10) 
+# print(20 >= 10) 
+# print(20 <= 10) 
+
