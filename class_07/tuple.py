5af630f41e25dc960d0d0e13214a30bcd53033f6
ghostofphantom <itsmehassxn@gmail.com>
2025-10-29 17:34:23 +0500

    Tuple

---
+++ class_07/tuple.py
@@ -0,0 +1,20 @@
+
+# my_tuple = ("Hello", 88, "adsfasdf")
+
+# mynum1 = (1,3,4,5,6)
+# mynum2 = (1,2) * 5
+
+# result = mynum1 + mynum2
+
+myvalues = (1,2,3,4,5,6,7677)
+
+# print(min(myvalues))
+# print(max(myvalues))
+# print(sum(myvalues))
+
+# print(myvalues[-1])
+
+# SLICING
+
+# print(myvalues[2 : 6])
+print(myvalues[:4])
