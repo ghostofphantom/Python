c3ff7a8845a9c8cf64849594daa8372f3de83fff
ghostofphantom <itsmehassxn@gmail.com>
2025-10-29 14:04:17 +0500

    ghostofphantom

--- project_01/qr_code_generator.py
+++ project_01/qr_code_generator.py
@@ -1,5 +1,8 @@
 import qrcode as qr
 
-img = qr.make("https://maps.app.goo.gl/o63UvYK82Ce7Z5Wt7")
+# img = qr.make("https://maps.app.goo.gl/o63UvYK82Ce7Z5Wt7")
 
-img.save("location.png")
+# img.save("location.png")
+
+git = qr.make("https://github.com/ghostofphantom/Python")
+git.save("ghostofphantom.png")
