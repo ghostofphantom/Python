0bce0476317afa0d129f275481ff498a2eb40ffb
ghostofphantom <itsmehassxn@gmail.com>
2025-11-05 17:58:10 +0500

    function

---
+++ class_10/shorthand.py
@@ -0,0 +1,9 @@
+
+
+
+# a = 10
+# b = 56
+
+# if a > b: print("A is Greater than b")
+
+# print("A is greater than B") if a > b else print("This is false statement")
---
+++ class_11/funcion.py
@@ -0,0 +1,26 @@
+
+
+# name = 10  GLOBA VARBIALE
+
+
+
+def Hello(mark1 , mark2):
+    # print("This is a function")
+    print(mark1 + mark2)
+
+Hello(10 , 10)
+Hello(5 , 6)
+Hello(5 , 64)
+
+
+
+# def exm():
+#     print("DO THIS")
+
+# exm()
+# exm()
+# exm()
+# exm()
+# exm()
+# exm()
+# exm()
